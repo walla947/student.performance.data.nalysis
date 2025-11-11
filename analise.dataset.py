af318ea3c63b7afeb57900d6580b556001d0a52c
Wallace Campos da Silva <127878337+walla947@users.noreply.github.com>
2025-11-11 09:54:16 -0300

    Update analise.dataset.py

--- analise.dataset.py
+++ analise.dataset.py
@@ -7,7 +7,7 @@ df = pd.read_csv("StudentsPerformance.csv")
 
 print("Pré-visualização do dataset:\n", df.head(), "\n")
 
-# Cria colunas derivadas
+
 df["average_score"] = df[["math score", "reading score", "writing score"]].mean(axis=1)
 df["performance_level"] = pd.cut(
     df["average_score"],
@@ -68,6 +68,7 @@ resumo = pd.DataFrame([resumo_coluna(c) for c in colunas])
 pd.set_option("display.max_columns", None)
 print(resumo)
 
-# Salva os resultados num arquivo CSV
+
 resumo.to_csv("resumo_estatistico.csv", index=False)
 print("\nResumo salvo em 'resumo_estatistico.csv'")
+
